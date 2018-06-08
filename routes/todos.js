09fc108dab54d076856d10890f29e84b1ff66ed1
Elijah Kennedy <kennedye2@mymail.vcu.edu>
2018-06-08 14:03:24 +0000

    post adds to database and sends back req.body perfectly

--- routes/todos.js
+++ routes/todos.js
@@ -13,7 +13,13 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-    console.log(req.body);
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.json(newTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    })
 });
 
 module.exports = router;
