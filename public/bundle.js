0b3e9b65fcd2ce429980c8cfcd9ef0644ad83ab0
Elijah Kennedy <kennedye2@mymail.vcu.edu>
2018-06-19 01:43:17 +0000

    back to click functionality on map while using browserify

--- public/bundle.js
+++ public/bundle.js
@@ -13,11 +13,41 @@ loadGoogleMapsApi({key: gmaps_key}).then(function (googleMaps) {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8
   })
-}).catch(function (error) {
-  console.error(error)
+      .addListener('click', function(e) {
+          var latLng = e.latLng;
+          var lat = latLng.lat();
+          var lng = latLng.lng();
+          if (confirm("Hey my guy! Would you like to log the coordinates of this location?")) {
+              console.log(lng);
+              console.log(lat);
+              createFlame(lat, lng);
+          } else {
+              console.log("cancelled logging of coords");
+          }
+        })
+    })
+    .catch(function (error) {
+      console.error(error)
 });
 
+/* global $ */
+$(document).ready(function(){
+ console.log("DA DOKKY IS REDDY");
+ $.getJSON('api/flames')
+ .then(function(data){
+     console.log(data);
+ });
+});
 
+function createFlame(lat, lng){
+$.post('https://www.squarrow.icu/api/flames', {lat: lat, lng: lng})
+.then(function(newFlame){
+  console.log(newFlame);
+})
+.catch(function(err){
+  console.log(err);
+});
+}
 
 },{"./rootURL":4,"jquery":2,"load-google-maps-api":3}],2:[function(require,module,exports){
 /*!
