cab0d915f25aff46895550543c18cbe75d06bb92
Elijah Kennedy <kennedye2@mymail.vcu.edu>
2018-06-19 03:31:41 +0000

    displays marker, gmaps API code much more intuitive/clean

--- public/index.js
+++ public/index.js
@@ -1,49 +1,44 @@
 var $ = require('jquery');
 var rootURL = require('./rootURL');
 console.log(rootURL);
-
-const loadGoogleMapsApi = require('load-google-maps-api');
-var gmaps_key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
-
+ 
+var loadGoogleMapsApi = require('load-google-maps-api-2');
+/*note that the googleMaps object this npm package returns
+is equivalent to the object that google.maps would return if using
+the googlemaps API directly from google.
+Therefore, googleMaps.maps will return an error */
+loadGoogleMapsApi.key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
 var map;
 
-loadGoogleMapsApi({key: gmaps_key}).then(function (googleMaps) {
-  map = new googleMaps.Map(document.querySelector('#map'), {
-    center: { lat: -34.397, lng: 150.644 },
+var newMap = function(gmapObject){
+  //create map
+    map = new gmapObject.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
     zoom: 8
-  })
-      .addListener('click', function(e) {
-          var latLng = e.latLng;
-          var lat = latLng.lat();
-          var lng = latLng.lng();
-          if (confirm("Hey my guy! Would you like to log the coordinates of this location?")) {
-              console.log(lng);
-              console.log(lat);
-              createFlame(lat, lng);
-          } else {
-              console.log("cancelled logging of coords");
-          }
-        })
-    })
-    .catch(function (error) {
-      console.error(error)
+  });
+  
+  //create marker
+  var marker = new google.maps.Marker({
+  position: {lat: -34.397, lng: 150.644},
+  map: map
 });
-
-/* global $ */
-$(document).ready(function(){
- console.log("DA DOKKY IS REDDY");
- $.getJSON('api/flames')
- .then(function(data){
-     console.log(data);
- });
-});
-
-function createFlame(lat, lng){
-$.post('https://www.squarrow.icu/api/flames', {lat: lat, lng: lng})
-.then(function(newFlame){
-  console.log(newFlame);
-})
-.catch(function(err){
-  console.log(err);
-});
+  
 }
+ 
+loadGoogleMapsApi().then(function (googleMaps) {
+  console.log(googleMaps);
+  console.log("success");
+  newMap(googleMaps);
+  
+}).catch(function (err) {
+  console.error(err);
+});
+
+
+
+
+//const loadGoogleMapsApi = require('load-google-maps-api');
+//if not using it, remember to remove from npm
+
+//var gmaps_key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
+//gmaps url
