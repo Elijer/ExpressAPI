98dc86e58d70fe5247dc3ac47f8e74b30b5c6de9
Elijah Kennedy <kennedye2@mymail.vcu.edu>
2018-06-26 01:13:39 +0000

    changed map in newMap function to var map

--- public/index.js
+++ public/index.js
@@ -8,6 +8,7 @@ var loadGoogleMapsApi = require('load-google-maps-api-2');
 is equivalent to the object that google.maps would return if using
 the googlemaps API directly from google.
 Therefore, googleMaps.maps will return an error */
+
 loadGoogleMapsApi.key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
 var map;
  
@@ -20,14 +21,16 @@ loadGoogleMapsApi().then(function (googleMaps) {
   console.error(err);
 });
 
+
 var newMap = function(gmapObject){
   //create map
-    map = new gmapObject.Map(document.getElementById('map'), {
+    var map = new gmapObject.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
   return map;
 };
+
 
 var newMarker = function(){
   //create marker
@@ -61,7 +64,6 @@ var createFlame = function(lat, lng, rootUrl){
         console.log(err);
       });
 };
-    
 
 var getFlames = function(){
      $.getJSON('api/flames')
@@ -78,9 +80,6 @@ var getFlames = function(){
          });
      });
 }
-
-
-
 
 //const loadGoogleMapsApi = require('load-google-maps-api');
 //if not using it, remember to remove from npm
