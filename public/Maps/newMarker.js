afd9d430d1ca809d82eb53978a9edc864fdef9ec
Elijah Kennedy <kennedye2@mymail.vcu.edu>
2018-06-26 03:09:57 +0000

    gif sticker for marker works

--- public/Maps/newMarker.js
+++ public/Maps/newMarker.js
@@ -1,7 +1,13 @@
 var newMarker = function(googleMaps, lat, lng, targetMap){
     var shmoooop = new googleMaps.Marker({
         position: {lat: lat, lng: lng},
-        map: targetMap
+        map: targetMap,
+        //optimized: false,
+        
+        icon: {url: "https://media.giphy.com/media/26BRt5hkD6hLzTl3q/giphy.gif",
+            scaledSize: new googleMaps.Size(30, 60)},
+        origin: new googleMaps.Point(0,0), // origin
+        anchor: new googleMaps.Point(0, 0)
     });
 }
 
--- public/index.js
+++ public/index.js
@@ -25,7 +25,4 @@ loadGoogleMapsApi().then(function (googleMaps) {
   console.error(err);
 });
 
-//const loadGoogleMapsApi = require('load-google-maps-api');
-//if not using it, remember to remove from npm
-
 //var gmaps_key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
