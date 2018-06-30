1397f9c38e3f700b8aa236c0508e8487b5f744b4
Kua <Kua@Elijahs-MBP.home>
2018-06-29 23:14:45 -0400

    swapped forEach function in getFlames to a for loop

--- public/Maps/getFlames.js
+++ public/Maps/getFlames.js
@@ -2,16 +2,21 @@ var $ = require('jquery');
 var newMarker = require('./newMarker');
 
 var getFlames = function(googleMaps, targetMap){
+    console.log("tryna something");
      $.getJSON('api/flames')
      .then(function(data){
-         window.localStorage.setItem('flames', JSON.stringify(data));
-         //var someData = JSON.parse(window.localStorage.getItem('flames'));
-         //console.log(someData);
-         data.forEach(function(e){
-             newMarker(googleMaps, e.lat, e.lng, targetMap, 1);
-         });
+       //console.log(data)
+       //console.log(data[0]);
+       //console.log(data.length);
+       for (var i = 0; i < data.length; i++){
+         console.log(data[i]);
+       };
+         /*data.forEach(function(e){
+           console.log(e);
+             //newMarker(googleMaps, e.lat, e.lng, targetMap, 1);
+         });*/
      });
-    
+
 };
 
 module.exports = getFlames;
