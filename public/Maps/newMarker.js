1d3b02a7b2f6a5ce2b0d19350cb0b7e4cfe7cf49
Kua <kennedye2@mymail.vcu.edu>
2018-07-02 23:04:50 -0400

    using scalingHandler for getFlames as well as during zoom event

--- public/Maps/newMarker.js
+++ public/Maps/newMarker.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var rootURL = require('../rootURL');
+var scalingHandler = require('./scalingHandler');
 
 var newMarker = function(googleMaps, lat, lng, targetMap, zoomLevel, id, masterArray, index){
 
@@ -8,12 +9,11 @@ var newMarker = function(googleMaps, lat, lng, targetMap, zoomLevel, id, masterA
       map: targetMap,
       icon: {url: "https://media.giphy.com/media/26BRt5hkD6hLzTl3q/giphy.gif",
       //icon: {url: "https://media.giphy.com/media/xUydljLrnX00Dm59dH/giphy.gif",
-      size: new googleMaps.Size(30, 55),
-      anchor: new googleMaps.Point(15, 45),
-      scaledSize: new googleMaps.Size(30, 55),
       title: 'Hello World!'},
       iterationID: id
     });
+
+    scalingHandler(googleMaps, map, masterArray[index]);
 
     masterArray[index].addListener('click', function() {
       $.ajax({
