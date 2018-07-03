cf00a6005e9b99c0cc3d30b78d78a3a3a5551399
Kua <Kua@Elijahs-MBP.home>
2018-07-02 21:21:54 -0400

    cleaned up getFlames

--- public/Maps/getFlames.js
+++ public/Maps/getFlames.js
@@ -2,20 +2,18 @@ var $ = require('jquery');
 var newMarker = require('./newMarker');
 
 var getFlames = function(googleMaps, targetMap, masterArray){
-    var zoomLvl = targetMap.zoom;
-    console.log("tryna something");
-     $.getJSON('api/flames')
-     .then(function(data){
-       //console.log(data)
-       //console.log(data[0]);
-       //console.log(data.length);
-       for (var i = 0; i < data.length; i++){
-         var current = data[i];
-         //console.log(data[i]);
-         newMarker(googleMaps, current.lat, current.lng, targetMap, zoomLvl, current._id, masterArray, i);
-       };
-     });
-
+  var zoomLvl = targetMap.zoom;
+  console.log("tryna something");
+   $.getJSON('api/flames')
+   .then(function(data){
+     for (var i = 0; i < data.length; i++){
+       var current = data[i],
+       lat = current.lat,
+       lng = current.lng,
+       id = current._id;
+       newMarker(googleMaps, lat, lng, targetMap, zoomLvl, id, masterArray, i);
+     };
+   });
 };
 
 module.exports = getFlames;
