14575eaa4158a25e36aca006d9db38e70dec0b06
Kua <kennedye2@mymail.vcu.edu>
2018-07-02 22:16:58 -0400

    simplified scalingHandler even more

--- public/Maps/scalingHandler.js
+++ public/Maps/scalingHandler.js
@@ -6,12 +6,10 @@ var scalingHandler = function(gMaps, map, array){
   map.addListener('zoom_changed', function() {
 
     var zoomLvl = map.zoom;
-    var exper = 128;
     var upperLimit = 18;
     var lowerLimit = 12;
+    var scaleTool;
 
-    //create scaleTool, a variable used to compensate a markers scale against the maps
-    //in such a way that the marker's scale remains constant with features on said map
     if (zoomLvl >= upperLimit){
       var scaleTool = Math.pow(2, ((upperLimit-8)*-1));
     } else if (zoomLvl <= lowerLimit) {
@@ -20,16 +18,32 @@ var scalingHandler = function(gMaps, map, array){
       var scaleTool = Math.pow(2, ((zoomLvl-8)*-1));
     };
 
+    var exper = 128;
     var markerX = 30/exper;
     var markerY = 55/exper;
     var anchorX = 15/exper;
     var anchorY = 45/exper;
 
+    fuego = {
+      size: new gMaps.Size(markerX/scaleTool, markerY/scaleTool),
+      scaledSize: new gMaps.Size(markerX/scaleTool, markerY/scaleTool),
+      anchor: new gMaps.Point(anchorX/scaleTool, anchorY/scaleTool)
+    }
+
     //iterator
     for (var i = 0; i < masterArray.length; i++ ) {
+      var current = array[i].icon;
+      current.size = fuego.size;
+      current.scaledSize = fuego.size;
+      //current.scaledSize, current.size = fuego.size;
+      current.anchor = fuego.anchor;
+
+
+      /*
       array[i].icon.size = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
       array[i].icon.scaledSize = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
       array[i].icon.anchor = new gMaps.Point(anchorX/scaleTool, anchorY/scaleTool);
+      */
     }
   });
 };
