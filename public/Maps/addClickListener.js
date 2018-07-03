b9494b033dfef81f8ab0bf3f1cccd705b435d6e1
Kua <kennedye2@mymail.vcu.edu>
2018-07-02 23:58:06 -0400

    got rid of mouseDown event cause it didn't work

--- public/Maps/addClickListener.js
+++ public/Maps/addClickListener.js
@@ -20,26 +20,6 @@ var addClickListener = function(gMaps, aMap, rootURL, array){
     }
     */
   });
-
-    aMap.addListener('mousedown', function(e) {
-    var latLng = e.latLng;
-    var lat = latLng.lat();
-    var lng = latLng.lng();
-
-    //no modal
-    createFlame(gMaps, aMap, lat, lng, rootURL, array);
-
-    //modal question
-    /*
-    if (confirm("Hey my guy! Would you like to log the coordinates of this location?")) {
-        console.log(lng);
-        console.log(lat);
-        createFlame(lat, lng, rootURL);
-    } else {
-        console.log("cancelled logging of coords");
-    }
-    */
-  });
 }
 
 module.exports = addClickListener;
