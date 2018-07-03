3dce686b4d51f5f1288d6df5e9f2966dbe8ad53d
Kua <Kua@Elijahs-MBP.home>
2018-07-02 22:10:32 -0400

    simplified scalingHandler

---
+++ public/Maps/markerTypes/fuego.js
@@ -0,0 +1,19 @@
+/*var calibration = 128;
+var markerX = 30/calibration;
+var markerY = 55/calibration;
+var anchorX = 15/calibration;
+var anchorY = 45/calibration;
+var zoomX, zoomY, zanchorX, zanchorY;
+var upperLimit = 18;
+var lowerLimit = 12;
+
+var calibration = 128;
+
+var fuego = function(gMaps){
+  size: new gMaps.Size(30/calibration, markerY/scaleTool),
+  scaledSize: 10,
+  anchor: 10
+};
+
+module.exports = fuego;
+*/
--- public/Maps/scalingHandler.js
+++ public/Maps/scalingHandler.js
@@ -1,36 +1,36 @@
+//var _fuego = require('./markerTypes/fuego');
+//var fuego = _fuego(gMaps);
+
 var scalingHandler = function(gMaps, map, array){
-  var zoomLvl = map.zoom;
-
-
-  var exper = 128;
-  var markerX = 30/exper;
-  var markerY = 55/exper;
-  var anchorX = 15/exper;
-  var anchorY = 45/exper;
-  var zoomX, zoomY, zanchorX, zanchorY;
-  var upperLimit = 18;
-  var lowerLimit = 12;
 
   map.addListener('zoom_changed', function() {
-        //console.log(masterArray);
-        var zoomLevel = map.zoom;
-        //getFlames(googleMaps, map);
-        console.log(zoomLevel);
-        if (zoomLevel >= upperLimit){
-          var scaleTool = Math.pow(2, ((upperLimit-8)*-1));
-        } else if (zoomLevel <= lowerLimit) {
-          var scaleTool = Math.pow(2, ((lowerLimit-8)*-1));
-        } else {
-          var scaleTool = Math.pow(2, ((zoomLevel-8)*-1));
-        }
 
-        //scaleHandler(masterArray, googleMaps, scaleTool);
+    var zoomLvl = map.zoom;
+    var exper = 128;
+    var upperLimit = 18;
+    var lowerLimit = 12;
 
-        for (var i = 0; i < masterArray.length; i++ ) {
-          masterArray[i].icon.size = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
-          masterArray[i].icon.scaledSize = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
-          masterArray[i].icon.anchor = new gMaps.Point(anchorX/scaleTool, anchorY/scaleTool);
-        }
+    //create scaleTool, a variable used to compensate a markers scale against the maps
+    //in such a way that the marker's scale remains constant with features on said map
+    if (zoomLvl >= upperLimit){
+      var scaleTool = Math.pow(2, ((upperLimit-8)*-1));
+    } else if (zoomLvl <= lowerLimit) {
+      var scaleTool = Math.pow(2, ((lowerLimit-8)*-1));
+    } else {
+      var scaleTool = Math.pow(2, ((zoomLvl-8)*-1));
+    };
+
+    var markerX = 30/exper;
+    var markerY = 55/exper;
+    var anchorX = 15/exper;
+    var anchorY = 45/exper;
+
+    //iterator
+    for (var i = 0; i < masterArray.length; i++ ) {
+      array[i].icon.size = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
+      array[i].icon.scaledSize = new gMaps.Size(markerX/scaleTool, markerY/scaleTool);
+      array[i].icon.anchor = new gMaps.Point(anchorX/scaleTool, anchorY/scaleTool);
+    }
   });
 };
 
