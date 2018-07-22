51a9602cece3b495e4e04afe674172fbd177a967
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 17:25:57 -0400

    big refactor, step 2: brought addClickListener into index.js

--- public/index.js
+++ public/index.js
@@ -29,9 +29,31 @@ loadGoogleMapsApi().then(function (googleMaps) {
   };
   map = newMap(googleMaps);
 
-
-
   masterArray = [];
+
+  //addClickLister
+  var createFlame = require('./Maps/createFlame');
+  var addClickListener = function(gMaps, aMap, rootURL, array){
+      aMap.addListener('click', function(e) {
+      var latLng = e.latLng;
+      var lat = latLng.lat();
+      var lng = latLng.lng();
+
+      //no modal
+      createFlame(gMaps, aMap, lat, lng, rootURL, array);
+
+      //modal question
+      /*
+      if (confirm("Hey my guy! Would you like to log the coordinates of this location?")) {
+          console.log(lng);
+          console.log(lat);
+          createFlame(lat, lng, rootURL);
+      } else {
+          console.log("cancelled logging of coords");
+      }
+      */
+    });
+  }
   addClickListener(googleMaps, map, rootURL, masterArray);
   getFlames(googleMaps, map, masterArray);
 
