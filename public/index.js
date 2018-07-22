c147d5c97c9719d8755d5c319d6318c1abc259bd
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 17:46:00 -0400

    deleted a lot files

--- public/index.js
+++ public/index.js
@@ -1,10 +1,6 @@
 /*global googleMaps*/
 var $ = require('jquery'),
     rootURL = require('./rootURL'),
-    //newMap = require('./Maps/newMap'),
-    addClickListener = require('./Maps/addClickListener'),
-    getFlames = require('./Maps/getFlames'),
-    scalingHandler = require('./Maps/scalingHandler'),
     loadGoogleMapsApi = require('load-google-maps-api-2');//use googleMaps, not google.maps w/ this module
 loadGoogleMapsApi.key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
 /*if ("geolocation" in navigator) {
