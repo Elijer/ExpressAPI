b45b5eade7e73c38486d50c68d2543c104824da1
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 17:39:10 -0400

    big refactor 7: brought getFlames into index.js

--- public/index.js
+++ public/index.js
@@ -138,6 +138,27 @@ var createFlame = function(gMaps, map, lat, lng, rootUrl, array){
     });
   }
   addClickListener(googleMaps, map, rootURL, masterArray);
+
+  //getFlames
+  var doSetTimeout = function(googleMaps, lat, lng, targetMap, id, masterArray, i) {
+    setTimeout(function() {
+      newMarker(googleMaps, lat, lng, targetMap, id, masterArray, i);
+    }, i*20);
+  };
+
+  var getFlames = function(googleMaps, targetMap, masterArray){
+    var zoomLvl = targetMap.zoom;
+     $.getJSON('api/flames')
+     .then(function(data){
+       for (var i = 0; i < data.length; i++){
+         var current = data[i],
+         lat = current.lat,
+         lng = current.lng,
+         id = current._id;
+         doSetTimeout(googleMaps, lat, lng, targetMap, id, masterArray, i);
+       };
+     });
+  };
   getFlames(googleMaps, map, masterArray);
 
   map.addListener('zoom_changed', function(e) {
