fecf260bd98bd551c60178f4f5043a7fa3716c9e
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 19:00:57 -0400

    moved buildMap functionality out of geo file

--- public/geographicFunctionality/buildMap.js
+++ public/geographicFunctionality/buildMap.js
@@ -2,16 +2,9 @@ var $                   = require('jquery'),
     rootURL             = require('../rootURL'),
     loadGoogleMapsApi   = require('load-google-maps-api-2'), //use googleMaps, not google.maps w/ this module
     mapConfig           = require('./mapConfig'),
-    annotateMap         = require('./annotateMap'),
-    buildMap            = require('./buildMap');
+    andThenAnnotateIt     = require('./annotateMap');
 
-loadGoogleMapsApi.key   = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
-
-var geo = function(){
-  buildMap();
-};
-
-/*buildMap = function(){
+buildMap = function(){
   loadGoogleMapsApi().then(function (googleMaps) {
     var darkStyle = require('./mapStyles/dark');
     var newMap = function(googleMaps){
@@ -19,13 +12,12 @@ var geo = function(){
       return map;
     };
     map = newMap(googleMaps);
-
-    annotateMap(googleMaps, map);
+    andThenAnnotateIt(googleMaps, map);
 
   }).catch(function (err) {
     console.error(err);
   });
 
-}*/
+};
 
-module.exports = geo;
+module.exports = buildMap;
