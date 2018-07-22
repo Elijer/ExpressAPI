b69d5ca595dbb04617fee76000f2526d20c55909
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 14:05:49 -0400

    successfully staggered getFlames population

--- public/Maps/getFlames.js
+++ public/Maps/getFlames.js
@@ -1,5 +1,10 @@
 var $ = require('jquery');
 var newMarker = require('./newMarker');
+doSetTimeout = function(googleMaps, lat, lng, targetMap, id, masterArray, i) {
+  setTimeout(function() {
+    newMarker(googleMaps, lat, lng, targetMap, id, masterArray, i);
+  }, i*100);
+};
 
 var getFlames = function(googleMaps, targetMap, masterArray){
   var zoomLvl = targetMap.zoom;
@@ -10,7 +15,11 @@ var getFlames = function(googleMaps, targetMap, masterArray){
        lat = current.lat,
        lng = current.lng,
        id = current._id;
-       newMarker(googleMaps, lat, lng, targetMap, id, masterArray, i);
+       //console.log(i);
+       doSetTimeout(googleMaps, lat, lng, targetMap, id, masterArray, i);
+
+       //timeoutFunction(console.log(i), i*100);
+         //newMarker(googleMaps, lat, lng, targetMap, id, masterArray, i)
      };
    });
 };
