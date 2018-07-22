86c70451f3560fa53ddbbfb1d5f1656a56aeb086
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 17:34:26 -0400

    big refactor, step 5: brought scalingHandler.js into index.js

--- public/index.js
+++ public/index.js
@@ -31,12 +31,35 @@ loadGoogleMapsApi().then(function (googleMaps) {
 
   masterArray = [];
 
+//scaling Handler
+//var scalingHandler = require('./Maps/scalingHandler');
+var makeFuego = require('./Maps/markerTypes/fuego');
+var scalingHandler = function(gMaps, map, markerInstance){
+  var zoomLvl = map.zoom;
+  var upperLimit = 18;
+  var lowerLimit = 9;
+  var scaleTool;
+
+  if (zoomLvl >= upperLimit){
+    var scaleTool = Math.pow(2, ((upperLimit-8)*-1));
+  } else if (zoomLvl <= lowerLimit) {
+    var scaleTool = Math.pow(2, ((lowerLimit-8)*-1));
+  } else {
+    var scaleTool = Math.pow(2, ((zoomLvl-8)*-1));
+  };
+
+  var fuego = makeFuego(gMaps, scaleTool, 32);
+
+  //iterator
+    var current = markerInstance.icon;
+    current.size = fuego.size;
+    current.scaledSize = fuego.size;
+    current.anchor = fuego.anchor;
+};
+
 //newMarker
 //var newMarker = require('./Maps/newMarker');
-var scalingHandler = require('./Maps/scalingHandler');
-
 var newMarker = function(googleMaps, lat, lng, targetMap, id, masterArray, index){
-
     masterArray[index] = new googleMaps.Marker({
       position: {lat: lat, lng: lng},
       map: targetMap,
