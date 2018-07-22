ebf10a419d518b991ed1af022c3dc8f89f26a504
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 19:19:10 -0400

    moved fuego into own file

--- public/geographicFunctionality/annotate/annotateMap.js
+++ public/geographicFunctionality/annotate/annotateMap.js
@@ -1,26 +1,10 @@
 var rootURL             = require('../../rootURL');
 var $                   = require('jquery');
 
+var makeFuego           = require('./fuego');
+
 var annotateMap = function(googleMaps, map){
   masterArray = [];
-
-  //makeFuego
-  //var makeFuego = require('./Maps/markerTypes/fuego');
-  var makeFuego = function(googleMaps, scaleTool, calibrate){
-
-    var scale = calibrate*scaleTool,
-        markerX = 30/scale,
-        markerY = 55/scale,
-        anchorX = 15/scale,
-        anchorY = 45/scale;
-
-    fuego = {
-      size: new googleMaps.Size(markerX, markerY),
-      scaledSize: new googleMaps.Size(markerX, markerY),
-      anchor: new googleMaps.Point(anchorX, anchorY)
-    };
-    return fuego;
-  }
 
   //scaling Handler
   //var scalingHandler = require('./Maps/scalingHandler');
