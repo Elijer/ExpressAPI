b5259647cbf045512a322ae3351659446e0980a0
Kua <kennedye2@mymail.vcu.edu>
2018-07-22 17:17:43 -0400

    big refactor, step 1: brought newMap.js into index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 /*global googleMaps*/
 var $ = require('jquery'),
     rootURL = require('./rootURL'),
-    newMap = require('./Maps/newMap'),
+    //newMap = require('./Maps/newMap'),
     addClickListener = require('./Maps/addClickListener'),
     getFlames = require('./Maps/getFlames'),
     scalingHandler = require('./Maps/scalingHandler'),
@@ -12,11 +12,25 @@ loadGoogleMapsApi.key = 'AIzaSyBI6f3-WMTwlVP7CVhpKiMbVlWvgI0s1_E';
 } else {
   console.log("geo no");
 }*/
- 
-
 
 loadGoogleMapsApi().then(function (googleMaps) {
+  //newMap.js
+  var darkStyle = require('./Maps/mapStyles/dark');
+  var newMap = function(gmapObject){
+      var map = new gmapObject.Map(document.getElementById('map'), {
+      center: {lat: 38.875854, lng: -77.0981069},
+      zoom: 14,
+      styles: darkStyle,
+      backgroundColor: "#212121",
+      fullscreenControl: false,
+      streetViewControl: false
+    });
+    return map;
+  };
   map = newMap(googleMaps);
+
+
+
   masterArray = [];
   addClickListener(googleMaps, map, rootURL, masterArray);
   getFlames(googleMaps, map, masterArray);
