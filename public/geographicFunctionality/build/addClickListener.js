cb8742bd49538075f14282c0de225e3873ba86e9
Kua <kennedye2@mymail.vcu.edu>
2018-07-24 11:33:18 -0400

    deleted createFlame file and collapsed it into the addClickListener file

--- public/geographicFunctionality/build/addClickListener.js
+++ public/geographicFunctionality/build/addClickListener.js
@@ -1,12 +1,27 @@
 var $                   = require('jquery');
 var createFlame     = require('../annotate/createFlame');
+var newMarker           = require('../annotate/newMarker');
 
 var addClickListener = function(googleMaps, rootURL){
     map.addListener('click', function(e) {
     //google maps stores click lat & lng in a weird way;
     var lat = e.latLng.lat();
     var lng = e.latLng.lng();
-    createFlame(googleMaps, lat, lng, rootURL, );
+
+    var createFlame = function(googleMaps, lat, lng, rootUrl){
+          $.post(rootUrl + '/api/flames', {lat: lat, lng: lng})
+          .then(function(newFlame){
+            console.log(newFlame);
+            var newID = newFlame._id;
+            var position = masterArray.length;
+            newMarker(googleMaps, lat, lng, newID, position);
+          })
+          .catch(function(err){
+            console.log(err);
+          });
+    };
+
+    createFlame(googleMaps, lat, lng, rootURL);
 
     //modal question
     /*
