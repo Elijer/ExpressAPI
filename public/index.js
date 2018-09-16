1d9e73a229a8aab1932ab13d4fd167726fbad7da
Kua <kennedye2@mymail.vcu.edu>
2018-09-16 14:35:33 -0400

    added message for naoh and dustin

--- public/index.js
+++ public/index.js
@@ -3,3 +3,17 @@ var $                   = require('jquery'),
     geo                 = require('./geographicFunctionality/1_geo');
 
 geo();
+
+
+var didMessage = JSON.parse(window.localStorage.getItem('message'));
+if (!didMessage){
+  var fillerUp_Message = "Hey Noah and Dustin and whoever else! See how many posts you can make on the map. I wanna get this database all full so that I can see what happens when it has 5k posts on it or so.";
+  setTimeout(function(){
+    alert(fillerUp_Message);
+  }, 3500);
+  window.localStorage.setItem("message", true);
+  console.log("message had not been seen");
+} else {
+  window.localStorage.setItem("message", true);
+  console.log("message was already seen");
+}
