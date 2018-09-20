1ac7a792d09e62b9b1e73a5c6b0b420a317d522d
Kua <kennedye2@mymail.vcu.edu>
2018-09-20 01:13:05 -0400

    only render gifs when at or below render limit

--- public/geographicFunctionality/onZoomChange.js
+++ public/geographicFunctionality/onZoomChange.js
@@ -8,6 +8,8 @@ var onZoomChange = function(googleMaps){
     var newZoom = map.getZoom();
     console.log("So the old zoom was " + map.oldZoom + ", but the new zoom is " + newZoom);
 
+    var renderLimit = 10;
+
     var counter = 0;
     for (var i = 0; i < masterArray.length; i++ ) {
       var currentBounds = map.getBounds();
@@ -17,24 +19,7 @@ var onZoomChange = function(googleMaps){
     };
     console.log(counter);
 
-
-
-    /*if (currentBounds.contains(latLng)){
-      markerInstance.setVisible(true);
-      var fuego = makeFuego(googleMaps, scalingCoefficient);
-      var ruford = makeRuford(googleMaps, scalingCoefficient)
-      //iterator
-        var current = markerInstance.icon;
-        current.size = ruford.size;
-        current.scaledSize = ruford.size;
-        current.anchor = ruford.anchor;
-    } else {
-      markerInstance.setVisible(false);
-    }*/
-
-
-
-    if (newZoom >= 17){
+    if (counter <= 10){
       var scalingCoefficient = scaleCalculator(newZoom);
       var markerInstance;
       for (var i = 0; i < masterArray.length; i++ ) {
@@ -57,6 +42,43 @@ var onZoomChange = function(googleMaps){
         //gifArray[i].visible = true;
       }
     }
+
+
+
+    /*if (currentBounds.contains(latLng)){
+      markerInstance.setVisible(true);
+      var fuego = makeFuego(googleMaps, scalingCoefficient);
+      var ruford = makeRuford(googleMaps, scalingCoefficient)
+      //iterator
+        var current = markerInstance.icon;
+        current.size = ruford.size;
+        current.scaledSize = ruford.size;
+        current.anchor = ruford.anchor;
+    } else {
+      markerInstance.setVisible(false);
+    }*/
+
+
+
+    /*
+    if (newZoom >= 17){
+      var scalingCoefficient = scaleCalculator(newZoom);
+      var markerInstance;
+      for (var i = 0; i < masterArray.length; i++ ) {
+        markerInstance = masterArray[i];
+        //markerInstance.visible = true;
+        markerInstance.setVisible(true);
+        scale(googleMaps, markerInstance, scalingCoefficient);
+        gifArray[i].setMap(null);
+        //scaleAnimator(googleMaps, markerInstance, newZoom);
+      }
+    } else {
+      for (var i = 0; i < masterArray.length; i++ ) {
+        masterArray[i].visible = false;
+        gifArray[i].setMap(map);
+        //gifArray[i].visible = true;
+      }
+    }*/
     map.oldZoom = newZoom;
   });
 }
